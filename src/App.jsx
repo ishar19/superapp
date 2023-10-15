610852d316685db52ebee6824f9658919288702d
ishar19 <isharjain19@gmail.com>
2023-10-16 00:15:02 +0530

    checkpoint 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,17 @@
-import {Routes, Route, Link} from 'react-router-dom'
-import Home from './pages/Home'
-import Genre from './pages/Genre'
-import Movies from './pages/Movies'
-import Browse from './pages/Browse'
-import React from 'react'
-const App=()=> {
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import Genre from "./pages/Genre";
 
+import React from "react";
+const App = () => {
   return (
     <>
-    <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path="/genre" element={<Genre/>}/>
-        <Route path="/movies" element={<Movies/>}/>
-        <Route path='/browse' element={<Browse/>}/>
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/genre" element={<Genre />} />
+      </Routes>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
