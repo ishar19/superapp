5f0ac4ea1f1866dbf2c64a3acb3d75fc97be9c5b
ishar19 <isharjain19@gmail.com>
2023-10-16 15:28:38 +0530

    fix

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,18 +1,49 @@
-import List from "../components/Movies/List"
-import Profile from '../assets/profileSmall.png'
-const Movies = ()=>{
-    const movies = JSON.parse(window.localStorage.getItem("genres"))
-    return(
-        <>
-        <img src={Profile} style={{position:"absolute",top:"2vh",right:"3vw",height:"60px",width:"60px"}}/>
-        <div style={{width:"100vw",minHeight:"100vh",background:"black",overflowX:"hidden"}}>
-        <p style={{color:"green",fontSize:"3rem",margin:"1vw"}}>Super app</p>
-        <p style={{color:"white",fontSize:"2rem",margin:"3vw"}}>Entertainment according to your choice</p>
-        {movies.map((movie)=><List genre={movie}/>)}
-        </div>
-        </>
-    )
-}
+import List from "../components/Movies/List";
+import Profile from "../assets/profileSmall.png";
+import styles from "../components/Movies/List.module.css";
+const Movies = () => {
+  const movies = JSON.parse(window.localStorage.getItem("genres"));
 
+  return (
+    <>
+      <div
+        style={{
+          width: "100vw",
+          minHeight: "100vh",
+          background: "black",
+          overflowX: "hidden",
+          maxHeight: "100vh",
+        }}
+      >
+        <img
+          src={Profile}
+          style={{
+            position: "absolute",
+            top: "2vh",
+            right: "3vw",
+            height: "60px",
+            width: "60px",
+          }}
+        />
+        <p
+          style={{
+            color: "green",
+            fontSize: "3rem",
+            margin: "1vw",
+          }}
+          className={styles.header}
+        >
+          Super app
+        </p>
+        <p style={{ color: "white", fontSize: "2rem", margin: "2vw" }}>
+          Entertainment according to your choice
+        </p>
+        {movies.map((movie) => (
+          <List genre={movie} />
+        ))}
+      </div>
+    </>
+  );
+};
 
-export default Movies
+export default Movies;
